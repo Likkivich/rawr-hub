c24095235506eb848ef59de85dab644604a85da4
Likkivich <ilovevalkyrieogg@gmail.com>
2026-03-02 02:37:26 +0200

    Delete api/logout.js

--- api/logout.js
+++
@@ -1,8 +0,0 @@
-// api/logout.js
-export default function handler(req, res) {
-  // Удаляем cookie выставив Max-Age=0
-  res.setHeader('Set-Cookie',
-    'rawr_session=; Path=/; HttpOnly; Secure; SameSite=Lax; Max-Age=0'
-  );
-  res.redirect('/');
-}
